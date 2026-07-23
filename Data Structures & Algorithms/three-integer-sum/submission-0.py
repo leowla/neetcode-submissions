ca917eca7cfd0f00b854a9790d62dd66c27223c3
Leo Wang <lwan925@aucklanduni.ac.nz>
2026-07-24 11:11:23 +1200

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        triplets = []
+
+        for i in range(len(nums)):
+            # go to a new i value
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                # valid triplet = zero total
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    # ensure non duplicate
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+        return triplets
